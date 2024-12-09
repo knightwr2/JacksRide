6f96241169e6dd6ca3ee15bf1a6b42b9e2046587
Garnet <69827126+Garnet2020@users.noreply.github.com>
2024-12-08 23:47:41 -0600

    Update Driver.java

--- driverClasses/Driver.java
+++ driverClasses/Driver.java
@@ -1,1 +1,44 @@
+package driverClasses;
+
+public class Driver extends User {
+    private Vehicle vehicle;
+    private boolean isAvailable;
+
+    public Driver(String username, String name, Vehicle vehicle) {
+        super(username, name);
+        this.vehicle = vehicle;
+      
+      // Assume the driver is available 
+      this.isAvailable = true; 
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailability(boolean isAvailable) {
+        this.isAvailable = isAvailable;
+    }
+
+    public double calculateFare(double distance) {
+        return vehicle.calculateFare(distance); 
+    }
+
+    public void acceptRide(RideRequest request) {
+        if (isAvailable) {
+            System.out.println("Driver " + getName() + " has accepted the ride request.");
+            setAvailability(false); 
+        } else {
+            System.out.println("Driver " + getName() + " is not available.");
+        }
+    }
+
+    public void completeRide() {
+        System.out.println("Ride completed. Driver " + getName() + " is now available.");
+        setAvailability(true); 
+}
 
